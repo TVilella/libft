89554c111cf89e3cd6348cb5445c283c69d24363
TVilella <vilelladev@outlook.com>
2021-08-28 17:25:50 -0300

    update libft.h

--- libft.h
+++ libft.h
@@ -6,17 +6,20 @@
 /*   By: tvilella <tvilella@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 12:39:01 by tvilella          #+#    #+#             */
-/*   Updated: 2021/08/28 13:10:31 by tvilella         ###   ########.fr       */
+/*   Updated: 2021/08/28 17:20:03 by tvilella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stdlib.h>
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+int ft_toupper(int c);
+int ft_tolower(int c);
 
 #endif
