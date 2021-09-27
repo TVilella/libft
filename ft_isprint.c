79bde28f1807e7a31251a13b63f88ecba102f45d
TVilella <vilelladev@outlook.com>
2021-09-26 21:18:52 -0300

    fix ft_isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: tvilella <tvilella@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 13:09:58 by tvilella          #+#    #+#             */
-/*   Updated: 2021/08/28 13:13:27 by tvilella         ###   ########.fr       */
+/*   Updated: 2021/09/26 21:17:52 by tvilella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_isprint(int c)
 {
-	if (c > 32)
+	if (c < 32)
 		return (0);
-	if (c < 126)
+	if (c > 126)
 		return (0);
 	return (1);
 }
