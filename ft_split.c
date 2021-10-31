acc2552f4ce0a24c84fce39771309ef90098824d
TVilella <vilelladev@outlook.com>
2021-10-30 21:11:36 -0300

    Fix split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tvilella <tvilella@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 11:40:53 by tvilella          #+#    #+#             */
-/*   Updated: 2021/10/30 13:49:09 by tvilella         ###   ########.fr       */
+/*   Updated: 2021/10/30 21:11:22 by tvilella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,56 +33,41 @@ static int	ft_countstr(char const *str, char cut)
 	return (count);
 }
 
-void	ft_subcat(char *new, char const *str, char cut )
+static size_t	ft_lensubstr(char const *str, char cut)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (*(str + i) != cut && *(str + i) != '\0')
-	{
-		new[i] = str[i];
+	while (str[i] != '\0' && str[i] != cut)
 		i++;
-	}
-	new[i] = '\0';
-}
-
-void	ft_cat(char **new, char const *str, char cut, int n)
-{
-	size_t	len;
-	int		i;
-	int		j;
-	int		start;
-
-	i = 0;
-	j = 0;
-	len = 0;
-	start = 0;
-	while (*(str + j) != '\0' && i < n)
-	{
-		while (*(str + j) == cut)
-			j++;
-		start = j;
-		while (*(str + j) != cut && *(str + j) != '\0')
-		{
-			len++;
-			j++;
-		}
-		new[i] = malloc(sizeof(char) * (len + 1));
-		ft_subcat(new[i], (str + start), cut);
-		len = 0;
-		i++;
-	}
+	return (i);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**split;
 	int		split_num;
+	int		i;
+	int		j;
 
+	j = 0;
+	i = 0;
+	if (s == NULL)
+		return (NULL);
 	split_num = ft_countstr(s, c);
-	split = (char **)malloc(sizeof (char *) * (split_num + 1));
+	split = ft_calloc(sizeof (char *), split_num + 1);
 	if (split == NULL)
 		return (NULL);
-	ft_cat(split, s, c, split_num);
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != c && s[i] != '\0')
+		{
+			split[j] = ft_substr(s, i, ft_lensubstr((s + i), c));
+			j++;
+			i = i + ft_lensubstr((s + i), c);
+		}
+	}
 	return (split);
 }
